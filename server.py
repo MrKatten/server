946e1b4dd500e1f2a45b3116517155be77ecdc49
ученик <iskander.akhtaryanov@bk.ru>
2023-02-13 19:06:13 +0500

    2, 3, 4, 5 задачи

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -11,9 +15,8 @@ def index():
 
 @app.route('/training/<prof>')
 def training(prof):
-    ns = ['врач', 'доктор', 'учёный']
     it = ['инженер', 'строитель']
-    return render_template('training.html', title=prof, prof=prof, ns=ns, it=it)
+    return render_template('training.html', title=prof, prof=prof, it=it)
 
 
 @app.route('/list_prof/<list>')
@@ -32,6 +35,17 @@ def answer():
               'ready': 'True'}
     return render_template("auto_answer.html", anketa=anketa, title='Анкета')
 
+class EmergencyForm(FlaskForm):
+    username = StringField('id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_capitan = StringField('id капитана', validators=[DataRequired()])
+    password_capitan = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
+@app.route('/login')
+def login():
+    form = EmergencyForm()
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
